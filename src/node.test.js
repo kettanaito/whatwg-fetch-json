157846aa0f3daa8705244e06b2291077c664cce4
Artem Zakharchenko <kettanaito@gmail.com>
2020-12-17 09:43:41 +0100

    Asserts [object Array] request body

--- src/node.test.js
+++ src/node.test.js
@@ -22,3 +22,26 @@ test('produces [object Object] out of the non-stringified request body', async (
 
   expect(xhrMock.send).toBeCalledWith('[object Object]')
 })
+
+test('produces [object Array] out of the non-stringified request body', async () => {
+  let xhrMock = {
+    open: jest.fn(),
+    setRequestHeader: jest.fn(),
+    onreadystatechange: jest.fn(),
+    send: jest.fn(),
+    readyState: 4,
+    status: 200,
+  }
+
+  window.XMLHttpRequest = jest.fn(() => xhrMock)
+
+  fetchPolyfill('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: [1, 2, 3],
+  })
+
+  expect(xhrMock.send).toBeCalledWith('[object Array]')
+})
